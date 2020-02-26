6c14b7a50f8fdff88e61325c07f81fc8ffed9459
Ram <ramonrails@gmail.com>
2020-02-26 19:40:15 +0530

    fix: square class reference

--- src/components/square.js
+++ src/components/square.js
@@ -1,24 +1,23 @@
 import React from "react";
 
-// class component
-//
-export default class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-// // class only had a "render" menthod, so converted to a function
-// // less lines of code with same result
+// // class component
 // //
-// export default function Square(props) {
-//   return (
-//     <button className="square" onClick={props.onClick}>
-//       {props.value}
-//     </button>
-//   )
+// export default class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
 // }
+
+// class with just `render` method, better be a function
+//
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
